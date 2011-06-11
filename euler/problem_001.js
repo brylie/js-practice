0f04997bc51b32cf716539667a772fa5e75a9640
Brylie Oxley <brylie@tuxfamily.org>
2011-06-11 00:32:02 +0000

    js clean up

--- euler/problem_001.js
+++ euler/problem_001.js
@@ -1,10 +1,10 @@
 /* Sum all natural numbers BELOW 100 that are divisible by 5 or 3 */
 var grand_total = 0;
 for (var i=0;i<1000;i++) {
-  if(i % 3 ==0) {
+  if(i % 3 === 0) {
     grand_total += i;
-  } else if (i % 5 ==0) {
+  } else if (i % 5 === 0) {
     grand_total += i;
-  };
-};
+  }
+}
 alert("Total: " + grand_total);
